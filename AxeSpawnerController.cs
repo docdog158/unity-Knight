668b03553135db42cf8331c551565abcf785644d
white_stone <150042192+docdog158@users.noreply.github.com>
2025-03-07 03:29:37 +0900

    Add files via upload

--- AxeSpawnerController.cs
+++ AxeSpawnerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SlashSpawnerController : BaseWeaponSpawner
+public class AxeSpawnerController : BaseWeaponSpawner
 {
     // 一度の生成に時差をつける
     int onceSpawnCount;
@@ -19,21 +19,16 @@ public class SlashSpawnerController : BaseWeaponSpawner
     void Update()
     {
         // タイマー消化
-        spawnTimer -= Time.deltaTime;
         if (isSpawnTimerNotElapsed()) return;
 
         // 偶数で左右に出す
         int dir = (onceSpawnCount % 2 == 0) ? 1 : -1;
 
-        // 場所
-        Vector3 pos = transform.position;
-        pos.x += 2f * dir;
-
         // 生成
-        SlashController ctrl = (SlashController)createWeapon(pos, transform);
+        AxeController ctrl = (AxeController)createWeapon(transform.position);
 
-        // 左右で角度を変える
-        ctrl.transform.eulerAngles = ctrl.transform.eulerAngles * dir;
+        // 斜め上に力を加える
+        ctrl.GetComponent<Rigidbody2D>().AddForce(new Vector2(100 * dir, 350));
 
         // 次の生成タイマー
         spawnTimer = onceSpawnTime;
